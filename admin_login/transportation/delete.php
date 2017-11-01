60d24acb9540fb6c5ddd19162dcc50e3c7328865
sumukh21 <31932355+sumukh21@users.noreply.github.com>
2017-11-01 15:41:12 +0530

    Add files via upload

---
+++ admin_login/transportation/delete.php
@@ -0,0 +1,12 @@
+<?php
+//including the database connection file
+include("config.php");
+ 
+//getting id of the data from url
+$id = $_GET['id'];
+//deleting the row from table
+$result = mysqli_query($mysqli, "DELETE FROM transports WHERE id=$id");
+ 
+//redirecting to the display page (index.php in our case)
+header("Location:index.php");
+?>
